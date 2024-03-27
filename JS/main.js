8668c152b35b6eac4efe776cabc768ec9aecf748
Rauf Abbasbeyli <raufabbasbeyli@gmail.com>
2024-03-27 16:09:54 +0400

    js-2

---
+++ JS/main.js
@@ -0,0 +1,137 @@
+
+// 1. Daxil olunan ədədin sadə yoxsa mürəkkəb olduğunu tapın.
+
+// let num = +prompt("Ədəd daxil et");
+
+// if(num % 2=== 0 )
+// {
+//     console.log(`${num} mürəkkəb ədəddir`)
+// }
+// else{
+//     console.log(`${num} sadə ədəddir`)
+    
+// }
+
+// 2. Daxil olunan ədədin mükəmməl olub olmadığını tapın.
+
+
+// let sun = +prompt("Ədəd daxil et");
+// let m= 0
+
+// for( i = 1; i < sun; i++) {
+//     if (sun % i === 0) {   
+
+//         m=m+i
+        
+//     } 
+//     }
+//     if(m===sun){
+//         console.log( `${sun} mukemmel ededdir`)
+//     }
+//     else{
+//         console.log( `${sun} mukemmel eded deyil`)
+//     }
+   
+    
+
+// 3. Daxil olunan 2 ədəd arasındakı mükəmməl ədədləri göstərin.
+
+
+// let first = +prompt("1-ci Ədədi daxil edin");
+// let second = +prompt("2-ci Ədədi daxil edin");
+// let m= 0
+// let n=0
+
+//       for( i = 1; i < first; i++) {
+//           if (first % i === 0) {   
+
+//               m=m+i
+        
+//           } 
+//           }
+//           if(m===first){
+//               console.log( `${first} mukemmel ededdir`)
+//           }
+//           else{
+//               console.log( `${first} mukemmel eded deyil`)
+//           }
+
+//           for( i = 1; i < second; i++) {
+//             if (second % i === 0) {   
+  
+//                 n=n+i
+          
+//             } 
+//             }
+
+//             if(n===second){
+//                 console.log( `${second} mukemmel ededdir`)
+//             }
+//             else{
+//                 console.log( `${second} mukemmel eded deyil`)
+//             }
+
+
+
+
+
+
+
+// 4. Daxil olunan 2 ədəd arasındakı sadə ədədləri göstərin.
+
+
+
+
+// let num = +prompt("1-ci ədədi daxil et");
+// let num2 = +prompt("2-ci ədədi daxil et");
+
+
+// if(num % 2 === 0 )
+// {
+//     console.log(`${num} sadə ədəddir`)
+// }
+// else{
+//     console.log(`${num} sadə ədəd deyil`)
+// }
+
+// if(num2 % 2 === 0){
+    
+//     console.log(`${num2} sadə ədəddir`)
+    
+// }
+// else{
+//     console.log(`${num2} sadə ədəd deyil`)
+// }
+
+
+
+
+
+
+
+
+// // 5. Daxil olunan 2 ədəd arasındakı sonu 7 ilə bitən ədədləri göstərin.
+
+
+// let n1 = +prompt("1-ci ədədi daxil et")
+// let n2 = +prompt("2-ci ədədi daxil et")
+
+
+
+// if(n1 === 7){
+//     console.log(`${n1} sonu 7 ile bitir`);
+// }
+// else{
+//     console.log(`${n1} sonu 7 ile bitmir`);
+
+// }
+// if(n2 ===7){
+//     console.log(`${n2} sonu 7 ile bitir`);
+// }
+// else{
+//     console.log(`${n2} sonu 7 ile bitmir`);
+
+// }
+
+
+
